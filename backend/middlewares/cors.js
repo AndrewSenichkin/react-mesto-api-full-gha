2e2aaed4759136630df1340e802e30006f192ed1
Andrew Senichkin <Kniws10@yandex.ru>
2023-06-12 22:48:21 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,22 +1,11 @@
 const allowedCors = ['https://kniws.nomoredomains.rocks', 'http://kniws.nomoredomains.rocks', 'https://localhost:3000', 'http://localhost:3000'];
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-  } if (method === 'OPTIONS') { // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
+  res.header('Access-Control-Allow-Origin', '*');
+  if (allowedCors === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     return res.end();
   }
-
-  return next();
+  next();
 };
