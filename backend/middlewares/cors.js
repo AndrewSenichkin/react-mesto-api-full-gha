4db1a2013c7fb00949d71de412fd43f5f29463b7
Andrew Senichkin <Kniws10@yandex.ru>
2023-06-12 22:54:46 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,8 @@
-const allowedCors = ['https://kniws.nomoredomains.rocks', 'http://kniws.nomoredomains.rocks', 'https://localhost:3000', 'http://localhost:3000'];
+// const allowedCors = ['https://kniws.nomoredomains.rocks', 'http://kniws.nomoredomains.rocks', 'https://localhost:3000', 'http://localhost:3000'];
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  if (allowedCors === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
     return res.end();
