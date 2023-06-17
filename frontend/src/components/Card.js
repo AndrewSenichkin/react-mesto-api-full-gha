07c80bb1544f6bd5e64aff66591adb34c8c3f800
Andrew Senichkin <Kniws10@yandex.ru>
2023-06-17 14:29:40 +0300

    корректировка api

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,10 +4,10 @@ import { CurrentUserContext } from '../context/CurrentUserContext';
 function Card(props) {
     const currentUser = React.useContext(CurrentUserContext);
     // Определяем, являемся ли мы владельцем текущей карточки
-    const isOwner = props.card.owner._id === currentUser._id;
+    const isOwner = (props.card.owner._id || props.card.owner) === currentUser._id
     //const deleteButtonClassName = `element__trash ${isOwner ? "element__trash_active" : ""}`
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = `element__smile ${isLiked ? 'element__smile_active' : ""}`;
 
--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,4 +1,4 @@
-class Api {
+/*class Api {
   constructor(options) {
       // тело конструктора
         this._baseUrl = options.baseUrl;
@@ -85,5 +85,115 @@ const api = new Api({
     authorization: `Bearer ${localStorage.getItem("jwt")}`,
   }
 });
+
+export default api;*/
+// Cоздание класса утилиты Api, для описания работы логики, обращения к Api
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl
+    this._headers = options.headers
+  }
+
+  // Формирую запрос на сервер, если прошел не удачно, возвращаем ошибку!
+  _handleSendingRequest(res) {
+    if (res.ok) {
+      return Promise.resolve(res.json())
+    }
+
+    // Если ошибка пришла, отклоняем промис
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  // Метод загрузки информации о пользователе с сервера
+  async getAboutUserInfo() {
+    const response = await fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод загрузки карточек с сервера
+  async getInitialCards() {
+    const response = await fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод редактирование профиля
+  async editProfileUserInfo(data) {
+    /*console.log(data)*/
+    const response = await fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод добавления новой карточки с сервера
+  async addNewCard(data) {
+    const response = await fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link,
+      }),
+    })
+
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод постановки лайка карточки
+  async addLike(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод постановки и снятия лайка с карточки
+  async deleteLike(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод удаления карточки
+  async deleteCard(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод обновления аватара пользователя
+  async updateProfileUserAvatar(data) {
+    const response = await fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatar,
+      }),
+    })
+    return this._handleSendingRequest(response)
+  }
+}
+
+const api = new Api({
+   baseUrl: "https://api.kniws.nomoredomains.rocks",
+   headers: {
+    "Content-Type": "application/json",
+    authorization: `Bearer ${localStorage.getItem("jwt")}`,
+  },
+})
 
 export default api;
